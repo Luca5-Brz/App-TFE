e36c2df86bbc9f297148a36874893d0db9d89cca
Lucas-Stage <lulubarrez@hotmail.com>
2022-03-09 16:18:44 +0100

    début UI

--- app/src/main/java/com/lucas/app_tfe/MainActivity.java
+++ app/src/main/java/com/lucas/app_tfe/MainActivity.java
@@ -1,14 +1,36 @@
 package com.lucas.app_tfe;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnBoissons;
+    Button mBtnRepas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnBoissons = findViewById(R.id.main_button_boisson);
+        mBtnRepas = findViewById(R.id.main_button_repas);
+        
+        
+        mBtnBoissons.setOnClickListener(view -> {
+            Toast.makeText(this, "Serveur Boissons", Toast.LENGTH_SHORT).show();
+        });
+        
+        mBtnRepas.setOnClickListener(view -> {
+            Toast.makeText(this, "Serveur Repas", Toast.LENGTH_SHORT).show();
+        });
+        
+        
+
     }
+
+
 }
